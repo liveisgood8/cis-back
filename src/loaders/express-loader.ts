57f6a45700253646453d88ea44306f7d4c65948a
Kirill <lemeshonok12@gmail.com>
2020-01-26 21:59:26 +0700

    Fix eslint for ts

--- src/loaders/express-loader.ts
+++ src/loaders/express-loader.ts
@@ -1,20 +1,22 @@
-import express, { Request, Response, NextFunction, ErrorRequestHandler, Errback } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import config from '../config';
 import { IStatus } from '../core/interfaces/status';
 
-export default () => {
+export default (): void => {
   const app = express();
   const development = process.env.NODE_ENV !== 'production';
 
   if (development) {
     console.log('application started in development mode');
-    app.get('/', (req: express.Request, res: express.Response) => {
+    app.get('/', (req: Request, res: Response) => {
       res.redirect('http://localhost:3000');
     });
   } else {
     app.use(express.static(config.staticPath));
   }
 
+  /* eslint-disable @typescript-eslint/no-unused-vars */
+  /* eslint-disable @typescript-eslint/no-explicit-any */
   app.use((req: Request, res: Response, next: NextFunction) => {
     const err: any = new Error('Указанного пути не существует');
     err['status'] = 404;
@@ -29,6 +31,9 @@ export default () => {
     res.status(err.status || 500)
         .json(status);
   });
+
+  /* eslint-enable @typescript-eslint/no-unused-vars */
+  /* eslint-enable @typescript-eslint/no-explicit-any */
 
   app.listen(config.expressPort, () => {
     console.log('express listening on port', config.expressPort);
