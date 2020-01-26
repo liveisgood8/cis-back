cc7473f83e618ef7e28678a5fc87c2bf64e911b1
Kirill <lemeshonok12@gmail.com>
2020-01-26 21:37:41 +0700

    Add user model

---
+++ src/models/user.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 64 })
+  login: string;
+
+  @Column()
+  password: string;
+
+  @Column({ nullable: true })
+  name: string;
+
+  @Column({ nullable: true })
+  surname: string;
+
+  @CreateDateColumn()
+  creationDate: Date;
+}
