37091f80c9a7c5f10d994d55b43e7cb0a03e84dc
Kirill <lemeshonok12@gmail.com>
2020-01-28 22:23:01 +0700

    app: remove log users

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,8 @@
 import loaders from './loaders';
-import { getRepository } from 'typeorm';
-import { User } from './models/user';
 
 
 async function main(): Promise<void> {
   await loaders();
-
-  const userRepo = getRepository(User);
-  console.log(await userRepo.find());
 }
 
 main().catch(console.error);
