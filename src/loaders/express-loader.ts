99ed99dd02ffe2463058349cb83cf4dda11213fe
Kirill <lemeshonok12@gmail.com>
2020-01-28 18:40:28 +0700

    express-loader fix type for error object

--- src/loaders/express-loader.ts
+++ src/loaders/express-loader.ts
@@ -24,7 +24,7 @@ export default (): void => {
   });
 
   /** Main error handler */
-  app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  app.use((err: Record<string, any>, req: Request, res: Response, next: NextFunction) => {
     const status: IStatus = {
       error: err.message,
     };
