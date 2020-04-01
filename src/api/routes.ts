d46f6c0af2f3d365448684c918d817df0f70b2ef
Lemeshonok Kirill <lemeshonok12@gmail.com>
2020-04-01 20:12:15 +0700

    update tsoa routes

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -189,7 +189,7 @@ const models: TsoaRoute.Models = {
     "IGetPendingNumberResponse": {
         "dataType": "refObject",
         "properties": {
-            "pendingNumber": { "dataType": "double", "required": true },
+            "pendingNumber": { "dataType": "integer", "required": true, "validators": { "isInt": { "errorMsg": "integer" } } },
         },
         "additionalProperties": false,
     },
@@ -270,27 +270,6 @@ export function RegisterRoutes(app: express.Express) {
             promiseHandler(controller, promise, response, next);
         });
     // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
-    app.post('/api/v1/auth/logout',
-        function(request: any, response: any, next: any) {
-            const args = {
-            };
-
-            // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
-
-            let validatedArgs: any[] = [];
-            try {
-                validatedArgs = getValidatedArgs(args, request);
-            } catch (err) {
-                return next(err);
-            }
-
-            const controller = new AuthController();
-
-
-            const promise = controller.logout.apply(controller, validatedArgs as any);
-            promiseHandler(controller, promise, response, next);
-        });
-    // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
     app.post('/api/v1/auth/register',
         function(request: any, response: any, next: any) {
             const args = {
@@ -339,7 +318,7 @@ export function RegisterRoutes(app: express.Express) {
         authenticateMiddleware([{ "JWT": [] }]),
         function(request: any, response: any, next: any) {
             const args = {
-                id: { "in": "path", "name": "id", "required": true, "dataType": "double" },
+                id: { "in": "path", "name": "id", "required": true, "dataType": "integer", "validators": { "isInt": { "errorMsg": "Id must be an integer" } } },
             };
 
             // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
