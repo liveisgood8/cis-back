ce55181b08c071e8f0968d380a145f76800930e9
Lemeshonok Kirill <lemeshonok12@gmail.com>
2020-06-25 20:03:50 +0700

    update front submodule

--- src/utils/mail/prod.ts
+++ src/utils/mail/prod.ts
@@ -3,7 +3,6 @@ import { createTransport } from 'nodemailer';
 
 export default (): Mail => {
   /** Make real transport for production */
-  console.log('smtp pass', process.env.SMTP_PASSWORD);
   return createTransport({
     host: 'smtp.yandex.ru',
     port: 465,
